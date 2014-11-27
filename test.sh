f1ea762ee2613161ce2c2a97d998543d58155f00
Philip Sahli <philip@sahli.net>
2014-11-27 22:28:07 +0100

    add sheebang, fix port in test

--- test.sh
+++ test.sh
@@ -1,11 +1,12 @@
+#!/bin/bash
 docker stop postgresql-test-test 2>/dev/null
 docker rm postgresql-test-test 2>/dev/null
 ID=`docker run -d -p 55432:5432 -e DB_NAME=test -e DB_USER=test -e PASSWORD=test --name postgresql-test-test postgresql-test`
 echo $ID
 sleep 8
 docker logs $ID
-set PGPASSWORD=test
-echo "select * from dual" | psql -h localhost -p 5432 -U test test 
+export PGPASSWORD=test
+echo "select * from dual" | psql -h localhost -p 55432 -U test test 
 rc=$?
 docker stop $ID 2>/dev/null
 docker rm $ID 2>/dev/null
