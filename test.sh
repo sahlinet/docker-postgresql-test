44c6c42770050cdc5813514a2b4173d013b5e773
philipsahli <philip@sahli.net>
2014-12-01 19:53:36 +0100

    add more sleeps to test

--- test.sh
+++ test.sh
@@ -3,7 +3,9 @@ docker stop postgresql-test-test 2>/dev/null
 docker rm postgresql-test-test 2>/dev/null
 ID=`docker run -d -p 49432:5432 -e DB_NAME=test -e DB_USER=test -e PASSWORD=test --name postgresql-test-test philipsahli/postgresql-test`
 echo $ID
-sleep 8
+sleep 6
+docker logs $ID
+sleep 6
 docker logs $ID
 export PGPASSWORD=test
 echo "SELECT 1 WHERE 1 = 0" | psql -h localhost -p 49432 -U test test 
